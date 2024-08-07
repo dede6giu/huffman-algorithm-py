286830af7a336187c3ee22d73fac274c06401709
José Antônio <85037819+dede6giu@users.noreply.github.com>
2024-08-07 20:44:28 -0300

    Create priorityqueue.py

---
+++ src/priorityqueue.py
@@ -0,0 +1,35 @@
+from typing import Any
+
+class PriorityQueue:
+    def __init__(self, 
+                 *,
+                 smaller: bool = True):
+        self.queue: list[tuple[Any, int]] = []
+        self.lt: bool = smaller
+    
+    def __str__(self):
+        return ' '.join([str(i) for i in self.queue])
+
+    def is_empty(self):
+        return len(self.queue) == 0
+
+    def enqueue(self, item: tuple[Any, int]):
+        if self.lt:
+            for i in range(len(self.queue)):
+                if item[1] < self.queue[i][1]:
+                    self.queue.insert(item, i)
+                    return
+            else:
+                self.queue.append(item)
+                return
+        else:
+            for i in range(len(self.queue)):
+                if item[1] > self.queue[i][1]:
+                    self.queue.insert(item, i)
+                    return
+            else:
+                self.queue.append(item)
+                return
+    
+    def dequeue(self):
+        return self.queue.pop(0)
