bcadb7c666707dc8b7d7429c87266c2f1f34af57
José Antônio <85037819+dede6giu@users.noreply.github.com>
2024-08-08 09:32:05 -0300

    development

--- src/hufffunc.py
+++ src/hufffunc.py
@@ -2,13 +2,25 @@ from hufftree import HuffTree
 from hufftree import NodeLetter
 from priorityqueue import PriorityQueue
 
-def create_hufftree(
-        node1: HuffTree | NodeLetter, 
-        node2: HuffTree | NodeLetter) -> HuffTree:
-    if int(node1) < int(node2):
-        return HuffTree(lchild=node1, rchild=node2)
-    else:
-        return HuffTree(lchild=node2, rchild=node1)
+def create_pq(chars: dict[str, int]) -> PriorityQueue:
+    # Creates priority queue of nodes 
+    # based on frequency, least first
+    pq = PriorityQueue()
+    for i in chars.keys():
+        pq.enqueue((NodeLetter(i, chars[i]), chars[i]))
+    return pq
+
+def create_hufftree(chars: dict[str, int]) -> HuffTree:
+    # Creates a hufftree from a table
+    # if empty -> empty HuffTree
+    #     else -> ready HuffTree
+    pq = create_pq(chars)
+    if pq.is_empty():
+        return HuffTree()
+    while len(pq) > 1:
+        aux = HuffTree(lchild=pq.dequeue(), rchild=pq.dequeue())
+        pq.enqueue((aux, aux.freq))
+    return pq.dequeue()
 
 def count_unique(text: str) -> dict[str, int]:
     # Finds all unique characters of a text
@@ -21,14 +33,10 @@ def count_unique(text: str) -> dict[str, int]:
             result[text[i]] += 1
     return result
 
-def create_pq(text: str) -> PriorityQueue:
-    # Creates priority queue of nodes 
-    # based on frequency, least first
-    chars: dict[str, int] = count_unique(text)
-    pq = PriorityQueue()
-    for i in chars.keys():
-        pq.enqueue((i, chars[i]))
-    return pq
+def compress_text(text: str) -> str:
+    charTable = count_unique(text)
+    tree = create_hufftree(text)
+    resultText = ''
 
-def compress_text(text: str):
-    ...
+    for i in range(len(text)):
+        ...
