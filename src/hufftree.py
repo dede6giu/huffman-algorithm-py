0fcb76e0976e48651ff9a6e489085c207f72541f
José Antônio <85037819+dede6giu@users.noreply.github.com>
2024-08-08 10:16:57 -0300

    development

--- src/hufftree.py
+++ src/hufftree.py
@@ -11,8 +11,11 @@ class NodeLetter:
 
 class HuffTree:
     def __init__(self, *, lchild=None, rchild=None):
-        self.left: NodeLetter = lchild
-        self.right: NodeLetter = rchild
+        self.check_children_integrity(lchild, rchild)
+        if rchild.freq < lchild.freq:
+            rchild, lchild = lchild, rchild
+        self.left: NodeLetter | HuffTree = lchild
+        self.right: NodeLetter | HuffTree = rchild
         self.freq: int = int(self.right) + int(self.left)
     
     def _fixfreq(self):
@@ -21,7 +24,37 @@ class HuffTree:
     def __int__(self):
         return self.freq
 
+    def check_children_integrity(self, left, right) -> None:
+        if not ((isinstance(right, NodeLetter) or isinstance(right, HuffTree)) and
+                (isinstance(left, NodeLetter) or isinstance(left, HuffTree))):
+            raise TypeError("Children are of wrong type, expected NodeLetter or HuffTree")
+        return None
+
     def set_children(self, lchild, rchild):
+        self.check_children_integrity(lchild, rchild)
+        if rchild.freq < lchild.freq:
+            rchild, lchild = lchild, rchild
         self.left = lchild
         self.right = rchild
         self._fixfreq()
+
+    def search_char(self, char: str) -> str:
+        queue: list[tuple[NodeLetter | HuffTree, str]] = [(self, '')]
+        while len(queue) != 0:
+            current = queue.pop(0)
+            if isinstance(current[0].left, NodeLetter) and current[0].left.char == char:
+                return current[1] + '0'
+            if isinstance(current[0].right, NodeLetter) and current[0].right.char == char:
+                return current[1] + '1'
+            
+            if isinstance(current[0].left, HuffTree):
+                queue.append((current[0].left, current[1] + '0'))
+            if isinstance(current[0].right, HuffTree):
+                queue.append((current[0].right, current[1] + '1'))
+
+            if isinstance(current[0].left, HuffTree) or isinstance(current[0].right, HuffTree):
+                continue
+
+            raise ValueError("self is not a valid HuffTree")
+        else:
+            raise KeyError(f"{char} not in tree")
